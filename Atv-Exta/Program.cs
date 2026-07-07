9ab6bd65d45deaf54abee804e10c455fd96ee6d0
andersonsimplicio <anderson25eu@gmail.com>
2026-07-07 17:12:23 -0300

    Ajste da variavel pos

--- Atv-Exta/Program.cs
+++ Atv-Exta/Program.cs
@@ -1,18 +1,15 @@
 ﻿class Program{
      public static void Main(){
-        const int tam = 30;
-        int[] vetor_orig = new int[tam] {5,5,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4};
+        const int tam = 60;
+        int[] vetor_orig = new int[tam] {5,5,8,9,9,9,7,9,6,4,5,9,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4,5,5,8,9,9,5,7,8,6,4};
         int[] memoria = new int[tam];
         int pos = 0,contador = 0,find=0;
         bool existe=false;
         for(int i=0; i< tam; i++){
-           
             find = vetor_orig[i];
-            //Console.Write($"\n {vetor_orig[i]}");
             for(int k=0;k<pos;k++){
                 if(find==memoria[k]){
                     existe=true;
-                    //Console.Write($"\n numero {find} ja existe");
                     break;
                 }else{
                     existe=false;
@@ -24,9 +21,11 @@
                 }
                 memoria[pos] = find;
                 Console.Write($"\n {find} {((double)contador / tam)*100:F1}%");
+                 pos++;
                 }
             contador = 0;
-            pos++;
+           
+            
         }
      }
      
